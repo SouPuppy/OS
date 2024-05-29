56dcb345bafc854c29840ad05c20d3ea73fbeab1
soupup <wdk1745@gmail.com>
2024-05-29 20:14:04 +0800

    introduce VGA palette

--- src/bootpack.c
+++ src/bootpack.c
@@ -1,15 +1,46 @@
-void io_hlt(void);
-void write_mem8(int addr, int data);
+#include "./H/GENERAL.H"
+#include "./H/COLOR.H"
 
-void HariMain(void) {
-    int i;
-    char *p;
-    p = (char *) 0xa0000;
-    for(i = 0; i <= 0xffff; i++) {
-        p[i] = i & 0x0f;
-    }
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
 
-    for(;;) {
-        io_hlt();
-    }
+void HariMain(void)
+{
+	char *vram;
+	int xsize, ysize;
+
+	init_palette();
+	vram = (char *) 0xa0000;
+	xsize = 320;
+	ysize = 200;
+
+	boxfill8(vram, xsize, scarlet,  0,         0,          xsize -  1, ysize - 29);
+	boxfill8(vram, xsize, white,  0,         ysize - 28, xsize -  1, ysize - 28);
+	boxfill8(vram, xsize, 2,  0,         ysize - 27, xsize -  1, ysize - 27);
+	boxfill8(vram, xsize, white,  0,         ysize - 26, xsize -  1, ysize -  1);
+
+	boxfill8(vram, xsize, 2,  3,         ysize - 24, 59,         ysize - 24);
+	boxfill8(vram, xsize, 2,  2,         ysize - 24,  2,         ysize -  4);
+	boxfill8(vram, xsize, 3,  3,         ysize -  4, 59,         ysize -  4);
+	boxfill8(vram, xsize, 3, 59,         ysize - 23, 59,         ysize -  5);
+	boxfill8(vram, xsize, 4,  2,         ysize -  3, 59,         ysize -  3);
+	boxfill8(vram, xsize, 4, 60,         ysize - 24, 60,         ysize -  3);
+
+	boxfill8(vram, xsize, 3, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
+	boxfill8(vram, xsize, 3, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
+	boxfill8(vram, xsize, 2, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
+	boxfill8(vram, xsize, 2, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+
+	for (;;) {
+		io_hlt();
+	}
+}
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
+{
+	int x, y;
+	for (y = y0; y <= y1; y++) {
+		for (x = x0; x <= x1; x++)
+			vram[y * xsize + x] = c;
+	}
+	return;
 }
--- src/naskfunc.nas
+++ src/naskfunc.nas
@@ -6,13 +6,47 @@
 [BITS 32]			
 [FILE "naskfunc.nas"]
 
-		GLOBAL	_io_hlt
-
+		GLOBAL	_io_hlt, _io_cli, _io_sti, _io_stihlt
+		GLOBAL  _io_load_eflags, _io_store_eflags
+		GLOBAL	_io_out_BYTE, _io_out_WORD, _io_out_DWORD
 
 ; functions
 
 [SECTION .text]
 
-_io_hlt:		; void io_hlt(void);
+_io_hlt:						; void io_hlt(void);
 		HLT
+		RET
+
+_io_cli:						; void io_hlt(void);
+		CLI
+		RET
+
+_io_load_eflags:				; int io_load_eflags(void);
+		PUSHFD		; PUSH EFLAGS
+		POP		EAX
+		RET
+
+_io_store_eflags:				; void io_store_eflags(int eflags);
+		MOV		EAX,[ESP + 4]
+		PUSH 	EAX
+		POPFD 		; POP EFLAGS
+		RET
+
+_io_out_BYTE:	; void io_out8(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		AL,[ESP+8]		; data
+		OUT		DX,AL
+		RET
+
+_io_out_WORD:	; void io_out16(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,[ESP+8]		; data
+		OUT		DX,AX
+		RET
+
+_io_out_DWORD:	; void io_out32(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,[ESP+8]		; data
+		OUT		DX,EAX
 		RET
