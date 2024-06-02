b5aa0f953d51a73cbf8c33224a300ba388e7f637
soupup <wdk1745@gmail.com>
2024-06-02 21:30:33 +0800

    makefile update

--- src/LIB/MATH/MATH.C
+++ src/LIB/MATH/MATH.C
@@ -1,3 +1,15 @@
 #include "MATH.H"
 
-void foo2();
+float inv_sqrt(float number) {
+    long i;
+    float x2, y;
+    const float threehalfs = 1.5F;
+
+    x2 = number * 0.5F;
+    y = number;
+    i = *(long*)&y;
+    i = 0x5f3759df - ( i >> 1);
+    y = *(float*)&i;
+    y = y * (threehalfs - (x2 * y * y));
+    return y;
+}
--- src/bootpack.c
+++ src/bootpack.c
@@ -2,31 +2,74 @@
 #include "./H/BOOTINFO.H"
 #include "./H/PALETTE.H"
 #include "./H/DISPLAY.H"
+#include "./H/IO.H"
 
-extern struct BOOTINFO *boot_info;
+#define NULL ((void*)0)
+
+FILE *stdin;
+FILE *stdout;
+FILE *stderr;
+
+FILE _stdin  = (FILE){ 0, NULL, 0, 0 };
+FILE _stdout = (FILE){ 1, NULL, 0, 0 };
+FILE _stderr = (FILE){ 2, NULL, 0, 0 };
+
+void init_IO() {
+    stdin  = &_stdin;
+    stdout = &_stdout;
+    stderr = &_stderr;
+}
+
+int _write(FILE *stream, char c) {
+    // boot_info->scrnx;
+    if (stream->_file == 1) { // stdout
+        static int x = 8, y = 8;
+        print_char(white, x, y, c);
+        x += 8;
+        // if (x >= boot_info->scrnx) {
+        //     x = 0;
+        //     y += 16;
+        // }
+        return 1;
+    }
+    return -1;
+}
+
+// extern struct BOOTINFO *boot_info;
+struct BOOTINFO *boot_info;
+void init_bootinfo() {
+	// see in asmhead.nas
+    boot_info = (struct BOOTINFO *) 0x0ff0;
+}
 
 void initiate(void) {
 	init_bootinfo();
 	init_palette();
+
+	init_IO();
 }
 
-
+extern FILE *stdin;
+extern FILE *stdout;
+extern FILE *stderr;
 
 void HariMain(void) {
 	initiate();
 	
 	box(black, 0, 0, boot_info->scrnx, boot_info->scrny);
-	
-	int i = 0;
-	print_char(white, i += 8, 8, foo('A'));
-	print_char(white, i += 8, 8, 'H');
-	print_char(white, i += 8, 8, 'N');
 
-	print_char(white, i += 8, 8, ' ');
+	_write(stdout, foo());
+	_write(stdout, 'A');
+	_write(stdout, 'A');
+	_write(stdout, 'A');
+	// print_char(white, i += 8, 8, 'Z');
+	// print_char(white, i += 8, 8, 'H');
+
+	// print_char(white, i += 8, 8, ' ');
 	
-	print_char(white, i += 8, 8, 'D');
-	print_char(white, i += 8, 8, 'S');
-	print_char(white, i += 8, 8, 'B');
+	// print_char(white, i += 8, 8, 'D');
+	// print_char(white, i += 8, 8, 'S');
+	// print_char(white, i += 8, 8, 'B');
 	
 	
 	for (;;) {
